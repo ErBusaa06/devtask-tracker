a5356c17accb5240dac5b4e6108b6604128f28c1
Miguel Angel Busatori Blanco <miguelangelbusatori.24@campuscamara.es>
2026-01-28 19:46:24 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config(); // carga las variables de entorno desde .env
+require('dotenv').config();
 
 const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json()); // para recibir JSON desde el frontend
+app.use(express.json());
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI)
